ca315d11744f63048b6dd726f4d1239db8612aea
nexarsoft <nexarsoft@gmail.com>
2026-03-22 01:53:07 -0300

    fix aws env strip

--- app.py
+++ app.py
@@ -19,14 +19,19 @@ print(">>> APP.PY CARREGADO:", __file__, flush=True)
 import boto3
 import os
 
+AWS_ACCESS_KEY_ID = (os.getenv("AWS_ACCESS_KEY_ID") or "").strip()
+AWS_SECRET_ACCESS_KEY = (os.getenv("AWS_SECRET_ACCESS_KEY") or "").strip()
+AWS_REGION = (os.getenv("AWS_REGION") or "").strip()
+AWS_BUCKET_NAME = (os.getenv("AWS_BUCKET_NAME") or "").strip()
+
+print("REGION DEBUG:", repr(AWS_REGION))
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
 )
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
